d10a39effb75c54dba91e163180f828113f2c4ee
thiagomeloo <4thiagomelo5@gmail.com>
2021-03-12 20:12:29 -0300

    initial commit

---
+++ src/main/java/com/siscontabil/siscontabil/SiscontabilApplication.java
@@ -0,0 +1,13 @@
+package com.siscontabil.siscontabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscontabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscontabilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siscontabil/siscontabil/SiscontabilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siscontabil.siscontabil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscontabilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

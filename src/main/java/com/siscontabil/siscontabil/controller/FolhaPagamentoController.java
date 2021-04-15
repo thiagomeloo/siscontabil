53ff0abda3722e671355f2341e378e87d103b0cf
thiagomeloo <4thiagomelo5@gmail.com>
2021-04-15 17:13:05 -0300

    add form contraCheque | update rotas #17

--- src/main/java/com/siscontabil/siscontabil/controller/FolhaPagamentoController.java
+++ src/main/java/com/siscontabil/siscontabil/controller/FolhaPagamentoController.java
@@ -1,17 +1,58 @@
 package com.siscontabil.siscontabil.controller;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import com.siscontabil.siscontabil.model.ContraCheque;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @Controller
 public class FolhaPagamentoController {
   
-  @RequestMapping("/folha-pagamento/create")
-    public String getIndex(){
-        return "pages/formFolhaPagamento";
+  @GetMapping("/folha-pagamento/create")
+  public String getFormFolhaPagamento(HttpSession session, Model model){
+    if(session.getAttribute("contracheques") == null){
+
+      model.addAttribute("allContraCheques", new ArrayList<ContraCheque>());
+
+    }else{
+
+      List<ContraCheque> contraCheques = contraCheques = (List<ContraCheque>) session.getAttribute("contracheques");
+      model.addAttribute("allContraCheques", contraCheques);
+
     }
+
+    return "pages/formFolhaPagamento";
+  }
+
+  @GetMapping("/folha-pagamento/add-contracheque")
+  public String getViewAddContraCheque(HttpSession session, Model model){
+    return "pages/formContraCheque";
+  }
+  
+  @PostMapping("/folha-pagamento/add-contracheque")
+  public String postAddContraCheque(HttpSession session, ContraCheque contraCheque){
+    if(session.getAttribute("contracheques") == null){
+      List<ContraCheque> contraCheques = new ArrayList<ContraCheque>();
+      contraCheques.add(contraCheque);
+      session.setAttribute("contracheques", contraCheques);
+    }else{
+      List<ContraCheque> contraCheques = (List<ContraCheque>) session.getAttribute("contracheques");
+      contraCheques.add(contraCheque);
+      session.setAttribute("contracheques", contraCheques);
+    }
+
+    return "redirect:/folha-pagamento/create";
+  }
 }
 
 

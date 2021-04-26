4331d04586e51d1b82324828995541de868a724f
thiagomeloo <4thiagomelo5@gmail.com>
2021-04-26 16:31:04 -0300

    autenticacao rotas setor e usuario

--- src/main/java/com/siscontabil/siscontabil/controller/SetorController.java
+++ src/main/java/com/siscontabil/siscontabil/controller/SetorController.java
@@ -1,7 +1,10 @@
 package com.siscontabil.siscontabil.controller;
 
+import javax.servlet.http.HttpSession;
+
 import com.siscontabil.siscontabil.model.Setor;
 import com.siscontabil.siscontabil.service.serviceImplents.SetorServiceImpl;
+import com.siscontabil.siscontabil.util.Autentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -19,43 +21,68 @@ public class SetorController {
   @Autowired
   SetorServiceImpl setorService;
 
+  Autentication auth = new Autentication();
+
   @GetMapping("/setor")
-  public ModelAndView getSetors() {
-    ModelAndView mv = new ModelAndView("pages/listaSetor");
-    mv.addObject("allSetors", setorService.findAll());
-    return mv;
+  public String getSetors(Model model, HttpSession session) {
+
+    String url = auth.getUrl(session, "pages/listaSetor");
+
+    if (auth.isAutenticated(session)) {
+      model.addAttribute("allSetors", setorService.findAll());
+    }
+
+    return url;
   }
 
   @GetMapping("/setor/create")
-  public String getFormCreate() {
-    return "pages/formSetor";
+  public String getFormCreate(HttpSession session) {
+    return (auth.getUrl(session, "pages/formSetor"));
   }
 
   @GetMapping("/setor/update/{id}")
-  public String getFormUpdate(@PathVariable("id") long id, Model model) {
-    try {
-      model.addAttribute("setor", setorService.findById(id));
-    } catch (Exception e) {
-      return HOME_PAGE;
+  public String getFormUpdate(@PathVariable("id") long id, Model model, HttpSession session) {
+
+    String url = auth.getUrl(session, "pages/formSetor");
+
+    if (auth.isAutenticated(session)) {
+      try {
+        model.addAttribute("setor", setorService.findById(id));
+      } catch (Exception e) {
+        return HOME_PAGE;
+      }
     }
-    return "pages/formSetor";
+
+    return url;
   }
 
   @PostMapping("/setor/create")
-  public String saveSetor(Setor setor, RedirectAttributes redirectAttributes) {
-    redirectAttributes.addAttribute("message_text", "Sucesso ao cadastrar o setor");
-    redirectAttributes.addAttribute("message_type", "success");
+  public String saveSetor(Setor setor, RedirectAttributes redirectAttributes, HttpSession session) {
 
-    setorService.save(setor);
-    return HOME_PAGE;
+    String url = auth.getUrl(session, HOME_PAGE);
+
+    if (auth.isAutenticated(session)) {
+      redirectAttributes.addAttribute("message_text", "Sucesso ao cadastrar o setor");
+      redirectAttributes.addAttribute("message_type", "success");
+
+      setorService.save(setor);
+    }
+
+    return url;
   }
 
   @PostMapping("/setor/update")
-  public String updateSetor(Setor setor, RedirectAttributes redirectAttributes) {
-    redirectAttributes.addAttribute("message_text", "Sucesso ao atualizar o setor");
-    redirectAttributes.addAttribute("message_type", "success");
-    setorService.save(setor);
-    return HOME_PAGE;
+  public String updateSetor(Setor setor, RedirectAttributes redirectAttributes, HttpSession session) {
+
+    String url = auth.getUrl(session, HOME_PAGE);
+
+    if (auth.isAutenticated(session)) {
+      redirectAttributes.addAttribute("message_text", "Sucesso ao atualizar o setor");
+      redirectAttributes.addAttribute("message_type", "success");
+      setorService.save(setor);
+    }
+
+    return url;
   }
 
 }
--- src/main/java/com/siscontabil/siscontabil/controller/UsuarioController.java
+++ src/main/java/com/siscontabil/siscontabil/controller/UsuarioController.java
@@ -1,42 +1,56 @@
 package com.siscontabil.siscontabil.controller;
 
+import javax.servlet.http.HttpSession;
+
 import com.siscontabil.siscontabil.model.Usuario;
 import com.siscontabil.siscontabil.service.serviceImplents.UsuarioServiceImpl;
+import com.siscontabil.siscontabil.util.Autentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UsuarioController {
-  
 
   @Autowired
   UsuarioServiceImpl usuarioService;
 
+  Autentication auth = new Autentication();
+
   @GetMapping("/usuario")
-  public ModelAndView getUsuarios(){
-    ModelAndView mv = new ModelAndView("pages/listaUsuario");
-      mv.addObject("allUsers", usuarioService.findAll());
-      return mv;
+  public String getUsuarios(Model model, HttpSession session) {
+
+    String url = auth.getUrl(session, "pages/listaUsuario");
+
+    if (auth.isAutenticated(session)) {
+      model.addAttribute("allUsers", usuarioService.findAll());
+    }
+
+    return url;
   }
 
   @GetMapping("/usuario/create")
-  public String getFormCreate(){
-      return "pages/formUsuario";
+  public String getFormCreate(HttpSession session) {
+    return (auth.getUrl(session, "pages/formUsuario"));
   }
-  
+
   @PostMapping(value = "/usuario/create")
-  public String saveUser(Usuario usuario,RedirectAttributes redirectAttributes){
-    redirectAttributes.addAttribute("message_text","Sucesso ao cadastrar o usuario");
-    redirectAttributes.addAttribute("message_type","success");
+  public String saveUser(Usuario usuario, RedirectAttributes redirectAttributes, HttpSession session) {
 
-      usuarioService.save(usuario); 
-      return "redirect:/";
+    String url = auth.getUrl(session, "redirect:/");
+
+    if (auth.isAutenticated(session)) {
+      redirectAttributes.addAttribute("message_text", "Sucesso ao cadastrar o usuario");
+      redirectAttributes.addAttribute("message_type", "success");
+
+      usuarioService.save(usuario);
+    }
+
+    return url;
   }
-
 
 }

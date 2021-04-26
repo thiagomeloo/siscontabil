a41090ea03e234ce27f86d3d7aac2087d94e0bc2
thiagomeloo <4thiagomelo5@gmail.com>
2021-04-26 16:19:27 -0300

    autenticacao rotas repotController | limpeza de imports

--- src/main/java/com/siscontabil/siscontabil/controller/ReportController.java
+++ src/main/java/com/siscontabil/siscontabil/controller/ReportController.java
@@ -2,8 +2,11 @@ package com.siscontabil.siscontabil.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import com.siscontabil.siscontabil.model.Movimentacao;
 import com.siscontabil.siscontabil.service.serviceImplents.MovimentacaoServiceImpl;
+import com.siscontabil.siscontabil.util.Autentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,29 +15,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReportController {
-  
 
   @Autowired
   MovimentacaoServiceImpl movimentacaoService;
 
+  Autentication auth = new Autentication();
+
   @GetMapping("/report/mensal")
-  public String getReport(Model model){
-    
+  public String getReport(Model model, HttpSession session) {
+
+    String url = auth.getUrl(session, "pages/reportLançamento");
+
+    if (auth.isAutenticated(session)) {
       List<Movimentacao> m = movimentacaoService.findAll();
-      double valorTotalEntrada =0;
-      double valorTotalSaida =0;
-      for(int i = 0;  i < m.size();i++ ){
-        if(m.get(i).getTipo().equals("Entrada")){
+      double valorTotalEntrada = 0;
+      double valorTotalSaida = 0;
+      for (int i = 0; i < m.size(); i++) {
+        if (m.get(i).getTipo().equals("Entrada")) {
           valorTotalEntrada += m.get(i).getValor();
-        }else{
+        } else {
           valorTotalSaida += m.get(i).getValor();
         }
-        
+
       }
-      model.addAttribute("valorTotalEntrada",valorTotalEntrada);
-      model.addAttribute("valorTotalSaida",valorTotalSaida);
+      model.addAttribute("valorTotalEntrada", valorTotalEntrada);
+      model.addAttribute("valorTotalSaida", valorTotalSaida);
       model.addAttribute("allReport", m);
-      return "pages/reportLançamento";
+    }
+
+    return url;
   }
 
 }

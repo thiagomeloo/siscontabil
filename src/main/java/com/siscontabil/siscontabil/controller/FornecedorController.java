12ef4c38d1f7137676d6f4de08a4bb62e7f68b80
thiagomeloo <4thiagomelo5@gmail.com>
2021-04-26 15:22:13 -0300

    autenticacao rotas fornecedor

--- src/main/java/com/siscontabil/siscontabil/controller/FornecedorController.java
+++ src/main/java/com/siscontabil/siscontabil/controller/FornecedorController.java
@@ -1,9 +1,12 @@
 package com.siscontabil.siscontabil.controller;
 
+import javax.servlet.http.HttpSession;
+
 import com.siscontabil.siscontabil.model.Endereco;
 import com.siscontabil.siscontabil.model.Fornecedor;
 import com.siscontabil.siscontabil.service.serviceImplents.EnderecoServiceImpl;
 import com.siscontabil.siscontabil.service.serviceImplents.FornecedorServiceImpl;
+import com.siscontabil.siscontabil.util.Autentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,70 +21,96 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class FornecedorController {
 
   private static final String HOME_PAGE = "redirect:/";
-  
-  
+
   @Autowired
   FornecedorServiceImpl fornecedorService;
 
   @Autowired
   EnderecoServiceImpl enderecoService;
 
+  Autentication auth = new Autentication();
+
   @GetMapping("/fornecedor/list")
-  public ModelAndView getListFornecedor(){
-    ModelAndView mv = new ModelAndView("pages/listFornecedor");
-    mv.addObject("allFornecedor", fornecedorService.findAll());
-    return mv;
+  public String getListFornecedor(HttpSession session, Model model) {
+
+    String url = auth.getUrl(session, "pages/listFornecedor");
+
+    if (auth.isAutenticated(session)) {
+      model.addAttribute("allFornecedor", fornecedorService.findAll());
+    }
+
+    return url;
   }
 
   @GetMapping("/fornecedor/create")
-  public ModelAndView getFormFornecedor(){
-    ModelAndView mv = new ModelAndView("pages/formFornecedor");
-    mv.addObject("allFornecedor", fornecedorService.findAll());
-    return mv;
+  public String getFormFornecedor(HttpSession session, Model model) {
+
+    String url = auth.getUrl(session, "pages/formFornecedor");
+
+    if (auth.isAutenticated(session)) {
+      model.addAttribute("allFornecedor", fornecedorService.findAll());
+    }
+
+    return url;
   }
 
-  @PostMapping({"/fornecedor/create"})
-  public String saveFuncionario( Fornecedor fornecedor,Endereco endereco, 
-   RedirectAttributes redirectAttributes){
-    redirectAttributes.addAttribute("message_text","Sucesso ao cadastrar o fornecedor");
-    redirectAttributes.addAttribute("message_type","success");
-    
-    Endereco e = enderecoService.save(endereco);
-    fornecedor.setEndereco(e);
-    fornecedorService.save(fornecedor);
-  
-    return HOME_PAGE;
+  @PostMapping({ "/fornecedor/create" })
+  public String saveFuncionario(Fornecedor fornecedor, Endereco endereco, RedirectAttributes redirectAttributes,
+      HttpSession session) {
+
+    String url = auth.getUrl(session, HOME_PAGE);
+
+    if (auth.isAutenticated(session)) {
+      redirectAttributes.addAttribute("message_text", "Sucesso ao cadastrar o fornecedor");
+      redirectAttributes.addAttribute("message_type", "success");
+
+      Endereco e = enderecoService.save(endereco);
+      fornecedor.setEndereco(e);
+      fornecedorService.save(fornecedor);
+    }
+
+    return url;
   }
 
   @GetMapping("/fornecedor/update/{id}")
-  public String getFormUpdate(@PathVariable("id") long id, Model model){
-    try {
-      model.addAttribute("fornecedor",fornecedorService.findById(id));
-    } catch (Exception e) {
-      return HOME_PAGE;
+  public String getFormUpdate(@PathVariable("id") long id, Model model, HttpSession session) {
+
+    String url = auth.getUrl(session, "pages/formFornecedor");
+
+    if (auth.isAutenticated(session)) {
+      try {
+        model.addAttribute("fornecedor", fornecedorService.findById(id));
+      } catch (Exception e) {
+        return HOME_PAGE;
+      }
     }
-    return "pages/formFornecedor";
+
+    return url;
   }
 
-  @PostMapping({"/fornecedor/update"})
-  public String updateFuncionario( Fornecedor fornecedor,Endereco endereco, 
-   RedirectAttributes redirectAttributes){
-    redirectAttributes.addAttribute("message_text","Sucesso ao atualizar o fornecedor");
-    redirectAttributes.addAttribute("message_type","success");
-    
-    if(fornecedor.isStatus() == false){
-      fornecedor.setStatus(fornecedor.isStatus());
-    }else{
-      fornecedor.setStatus(true);
+  @PostMapping({ "/fornecedor/update" })
+  public String updateFuncionario(Fornecedor fornecedor, Endereco endereco, RedirectAttributes redirectAttributes,
+      HttpSession session) {
+
+    String url = auth.getUrl(session, HOME_PAGE);
+
+    if (auth.isAutenticated(session)) {
+
+      redirectAttributes.addAttribute("message_text", "Sucesso ao atualizar o fornecedor");
+      redirectAttributes.addAttribute("message_type", "success");
+
+      if (fornecedor.isStatus() == false) {
+        fornecedor.setStatus(fornecedor.isStatus());
+      } else {
+        fornecedor.setStatus(true);
+      }
+      Endereco e = enderecoService.save(endereco);
+      fornecedor.setEndereco(e);
+      fornecedorService.save(fornecedor);
+      
     }
-    Endereco e = enderecoService.save(endereco);
-    fornecedor.setEndereco(e);
-    fornecedorService.save(fornecedor);
-  
-    return HOME_PAGE;
+
+    return url;
   }
 
 }
-
-
-
